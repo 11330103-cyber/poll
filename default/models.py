f07f1d87e878ac3b175a51d11bd3ce098bb7ad6b
Kinjin <11330103@dcsh.tp.edu.tw>
2025-11-21 11:12:19 +0800

    定義poll,option資料

--- default/models.py
+++ default/models.py
@@ -5,7 +5,14 @@ class Poll(models.Model):
     subject = models.CharField("投票主題",max_length=128)
     desc = models.TextField("說明")
     created = models.DateField("建立日期",auto_now_add=True)
+
+    def __str__(self):
+        return self.subject
 class Option(models.Model):
     title = models.CharField("選項文字",max_length=128)
     votes = models.IntegerField("票數",default=0)
     poll_id = models.IntegerField("投票主題編號")
+
+    def __str__(self):
+        #return "{}-{}".format(self.poll_id,self.title)
+        return f"{self.poll_id}-{self.title}:{self.votes}"
