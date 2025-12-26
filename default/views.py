799b258bd8bdc20d9f4512920ab5d4d17952a56a
Kinjin <11330103@dcsh.tp.edu.tw>
2025-12-26 12:07:09 +0800

    增加用戶識別

--- default/views.py
+++ default/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from .models import Poll,Option
 from django.views.generic import ListView,DetailView,RedirectView,CreateView,UpdateView,DeleteView
 from django.urls import reverse,reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
 def poll_list(req):
@@ -33,19 +34,19 @@ class pollvote(RedirectView):
         #return reverse('poll_view',args=[option.poll_id])
         return reverse('poll_view',kwargs={'pk':option.poll_id})
     
-class pollcreate(CreateView):
+class pollcreate(LoginRequiredMixin,CreateView):
     model=Poll
     fields='__all__'#['subject','desc']
     success_url=reverse_lazy('poll_list')
 
-class polledit(UpdateView):
+class polledit(LoginRequiredMixin,UpdateView):
     model=Poll
     fields='__all__'#['subject','desc']
 
     def get_success_url(self):
         return reverse('poll_view',kwargs={'pk':self.object.id})#改某個主題，到某個主體的頁面
 
-class optioncreate(CreateView):
+class optioncreate(LoginRequiredMixin,CreateView):
     model=Option
     fields=['title']
 
@@ -56,7 +57,7 @@ class optioncreate(CreateView):
     def get_success_url(self):
         return reverse_lazy('poll_view',kwargs={'pk': self.kwargs['pid']})
     
-class optionedit(UpdateView):
+class optionedit(LoginRequiredMixin,UpdateView):
     model=Option
     fields=['title']
     pk_url_kwarg='oid'  #讓他去讀取 oid
@@ -64,11 +65,11 @@ class optionedit(UpdateView):
     def get_success_url(self):
         return reverse_lazy('poll_view',kwargs={'pk': self.object.poll_id}) #self.object代表目前在操作的那筆紀錄
     
-class polldelete(DeleteView):
+class polldelete(LoginRequiredMixin,DeleteView):
     model=Poll
     success_url=reverse_lazy('poll_list')
 
-class optiondelete(DeleteView):
+class optiondelete(LoginRequiredMixin,DeleteView):
     model=Option
     
     def get_success_url(self):
