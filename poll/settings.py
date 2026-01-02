c10208d36584f28cfc0d30b98640162c1a36047a
Kinjin <11330103@dcsh.tp.edu.tw>
2026-01-02 12:08:01 +0800

    2026 0102

--- poll/settings.py
+++ poll/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-tl^-=z+)t5nhomg20!vt5s_xqgi=*dv6hy#gcaj2zq-ll_0u3f
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -116,6 +116,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
